6da57d89a87e20eefab20df601defed0e4c3bdd7
Hadrian Chio <hadrian.chio@gmail.com>
2022-11-19 10:56:01 +0100

    implemented class-validator and class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // implement ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Makes sure that incoming requests has only the defined DTO properties
+      // and ignores all other manual created properties
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
